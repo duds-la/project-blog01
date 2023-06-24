118922f288fdf265e24b6d3043a934957947dc4e
Eduardo A. Lazzaretti <110792669+duds-la@users.noreply.github.com>
2023-06-24 11:59:01 -0300

    Add files about project

---
+++ contato.php
@@ -0,0 +1,11 @@
+<?php
+    include_once("templates/header.php");
+
+?>
+    <h1>Página de contato</h1>
+
+    <h2>Ainda em construção :(</h2>
+    <?php
+    include_once("templates/footer.php");
+
+?>
